140e1930e757d7eb0a507e6b60f6f12b48d93802
lejanz <74564836+lejanz@users.noreply.github.com>
2022-02-22 00:28:36 -0800

    Update main.c

--- alpaca-bms-FE9_Rev2/alpaca-bms-FE9_Rev2.cydsn/main.c
+++ alpaca-bms-FE9_Rev2/alpaca-bms-FE9_Rev2.cydsn/main.c
@@ -21,10 +21,12 @@ int main(void)
     CyGlobalIntEnable; /* Enable global interrupts. */
 
     init();   //initialize modules
-    LTC6811_initialize(MD_FILTERED);
+    LTC6811_initialize(MD_FAST);
 
     uint8_t cfga[6];
-    uint16_t aux[16];
+    volatile uint8_t read_cfga[15][6];
+    uint16_t temp;
+    volatile uint16_t aux[16];
     uint16_t auxb[2][6];
     //uint8_t sel = 5;
     
@@ -35,12 +37,20 @@ int main(void)
         CyDelay(1);
         for (uint8_t sel = 0; sel < 16; sel++){
             LTC6811_wrcfga(1, sel, cfga);
-            CyDelay(1);
-            //LTC6811_rdcfga(1, cfga);
+            CyDelay(100);
+            LTC6811_rdcfga(1, read_cfga[sel]);
+            CyDelay(50);
             LTC6811_adax();
-            CyDelay(1);
-            LTC6811_rdaux_pin(1, GPIO5, &aux[sel]);
-            CyDelay(1);
+            CyDelay(100);
+            
+            if(LTC6811_rdaux_pin(1, GPIO5, &temp)){
+                BMS_OK_Write(0);
+                aux[sel] = 0xFFFF;
+            } else {
+            aux[sel] = temp;
+            }
+            
+            CyDelay(100);
         }
  
         //CyDelay(1);
